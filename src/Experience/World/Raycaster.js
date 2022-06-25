b99d6edd9c82208eb93bc8ff6d65219f06622e73
abdou abdou <abdou-elboursoumi@outlook.fr>
2022-06-25 12:14:22 +0200

    correction bug raycaster avec wheel

--- src/Experience/World/Raycaster.js
+++ src/Experience/World/Raycaster.js
@@ -27,30 +27,41 @@ export default class Raycaster {
         window.addEventListener('click', () => {
 
             // Si on a deja un object de selectionner
-            if (this.currentObjectSelected){
+            if (this.currentObjectSelected) {
+                //on remet l'objet a sa place initial
                 gsap.to(this.currentObjectSelected.position, { x: initialPosition.x, y: initialPosition.y, z: initialPosition.z, duration: 0.5, ease: 'power4.inOut' })
+                // on remet l'index de H1 à 1
+                gsap.to(this.heading1.style, { zIndex: 1, duration: 0.5, ease: 'power4.inOut' })
 
                 //SetTimeout pour attendre la fin de l'animation gsap
                 window.setTimeout(() => {
+                    //on remet la bonne rotation au mesh séléctionner
                     this.currentObjectSelected.lookAt(this.ImageDebugObject.lookAtVector)
+                    // on enleve l'objet selectionner
                     this.currentObjectSelected = null
                 }, 510)
-                
-                
+
+
 
             }
             //Si on a pas d'object deja séléctionner ET qu'on hover un object
             else if (this.currentIntersect) {
                 // on s'assure qu'on a pas déjà un object séléctionner 
-                if (this.currentObjectSelected == null){
+                if (this.currentObjectSelected == null) {
                     this.currentObjectSelected = this.currentIntersect.object
-                    initialPosition = new THREE.Vector3(this.currentIntersect.object.position.x, this.currentIntersect.object.position.y, this.currentIntersect.object.position.z) 
+                    initialPosition = new THREE.Vector3(this.currentIntersect.object.position.x, this.currentIntersect.object.position.y, this.currentIntersect.object.position.z)
                     gsap.to(this.heading1.style, { zIndex: -1, duration: 0.5, ease: 'power4.inOut' })
                     gsap.to(this.currentIntersect.object.position, { x: 0, y: 5, z: 1.25, duration: 1, ease: 'power4.inOut' })
                     this.currentIntersect.object.lookAt(this.currentIntersect.object.position)
                 }
 
 
+            }
+        })
+        // on destock l'object selectionner si on wheel comme ça si on reclique on a pas de bug
+        window.addEventListener('wheel', event => {
+            if (this.currentObjectSelected) {
+                this.currentObjectSelected = null
             }
         })
     }
@@ -95,15 +106,5 @@ export default class Raycaster {
 
             this.currentIntersect = null
         }
-
-        // for (const intersect of intersects) {
-        //     intersect.object.material.color.set('#0000ff')
-        // }
-
-        // for (const object of objectsToTest) {
-        //     if (!intersects.find(intersect => intersect.object === object)) {
-        //         object.material.color.set('#ff0000')
-        //     }
-        // }
     }
 }
